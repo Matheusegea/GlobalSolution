cc8b383d6e9ace231c80a3009c36780b4db4e831
matheusegea <matheusegea220504@gmail.com>
2025-06-05 05:03:27 -0300

    adicionando tema.js para mudar tema

---
+++ src/js/temas.js
@@ -0,0 +1,9 @@
+function switchTheme(theme) {
+  document.documentElement.setAttribute('data-theme', theme);
+  localStorage.setItem('theme', theme);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const savedTheme = localStorage.getItem('theme') || 'light';
+  switchTheme(savedTheme);
+});
